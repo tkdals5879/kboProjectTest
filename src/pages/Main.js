551956839939ba6f8cdd1c673fbc42bea3afa1bf
tkdals5879 <tkdals58799@gmail.com>
2025-05-06 17:36:36 +0900

    stadium pic - change , main> hover event

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { toggleFavorite } from '../redux/slice/teamsSlice'
+import { useNavigate } from 'react-router-dom'
 
 import '../css/main/main.css'
 
@@ -10,11 +11,20 @@ import '../css/main/main.css'
 
 function Main() {
 
+  const navigate = useNavigate();
+
   const dispatch = useDispatch()
   const teams = useSelector((state) => state.teams.teams)
 
   const favToogle = (teamId) => {
     dispatch(toggleFavorite(teamId))
+  }
+
+  const gotoStadium = (teamId) => {
+    navigate(`/stadium/${teamId}`)
+    window.scrollTo({
+      top:0
+    })
   }
 
   return (
@@ -25,7 +35,7 @@ function Main() {
 
         <div className='clubWrap'>
           {teams.map(team => (
-            <div key={team.id}>
+            <div key={team.id} onClick={() => gotoStadium(team.id)}>
               <img src={`.${team.logo}`} alt={team.name + 'logo'} />
               <button className='favorite' onClick={() => favToogle(team.id)}><FontAwesomeIcon icon={faStar} className={team.isFavorite ? 'fav' : ''} /></button>
             </div>
